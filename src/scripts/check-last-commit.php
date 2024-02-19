2fda88835ca1d2137caaa658691d3db82e2db598
Petr Krehlik <73128826+petr6@users.noreply.github.com>
2024-02-19 07:55:41 +0100

    fix(check-commit): skip merge commits

--- src/scripts/check-last-commit.php
+++ src/scripts/check-last-commit.php
@@ -1,7 +1,13 @@
 <?php
 
 // Načtení zprávy z posledního commitu
-$commitMessage = shell_exec('git log -1 --format=%B');
+$commitMessage = \shell_exec('git log -1 --format=%B');
+
+$loweredMessage = \strtolower($commitMessage);
+
+if (\str_starts_with($loweredMessage, 'merge')) {
+	exit(0);
+}
 
 // Ověření formátu conventional commits
 $pattern = '/^(feat|fix|docs|style|refactor|test|chore|perf|build|ops)(\([^)]+\))?!?: ((.|\n)+)/';
