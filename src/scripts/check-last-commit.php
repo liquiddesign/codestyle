d540efa0fca6cedba34207db68c9bb614ae79fdf
Petr Krehlik <73128826+petr6@users.noreply.github.com>
2024-02-23 10:27:58 +0100

    build(Commits): check all commits between last 2 tags

--- src/scripts/check-last-commit.php
+++ src/scripts/check-last-commit.php
@@ -1,26 +1,60 @@
 <?php
 
-/* @TODO
- * if you use conventional changelog, real commits are hidden with release commit
- * modify script to check all commits between two tags
-*/
-$commitMessage = \shell_exec('git log -1 --format=%B');
 
-$loweredMessage = \strtolower($commitMessage);
+// Funkce pro získání seznamu všech tagů, seřazených od nejnovějších
+function getGitTags() {
+	$output = shell_exec('git tag --sort=-v:refname');
+	$tags = explode("\n", trim($output));
+	return $tags;
+}
 
-if (\str_starts_with($loweredMessage, 'merge')) {
+// Funkce pro získání seznamu commitů mezi dvěma tagy
+function getCommitsBetweenTags($tag1, $tag2) {
+	$output = shell_exec('git log --pretty=format:"%H,%s" ' . $tag1 . '...' . $tag2);
+	$commits = explode("\n", trim($output));
+	return $commits;
+}
+
+// Získání seznamu všech tagů
+$tags = getGitTags();
+
+$pattern = '/^(feat|fix|docs|style|refactor|test|chore|perf|build|ops)(\([^)]+\))?!?: ((.|\n)+)/';
+$invalidCommits = [];
+
+// Pokud jsou k dispozici minimálně dva tagy, získáme commit mezi nimi
+if (count($tags) >= 2) {
+	$latestTag = $tags[0];
+	$previousTag = $tags[1];
+
+	echo "Poslední dva tagy: $latestTag, $previousTag\n";
+
+	// Získání seznamu commitů mezi dvěma tagy
+	$commits = getCommitsBetweenTags($previousTag, $latestTag);
+
+	// Vypsání commitů
+	echo "Commity mezi tagy $previousTag a $latestTag:\n";
+
+	foreach ($commits as $commit) {
+		[$id, $commitMessage] = \explode(',', $commit);
+
+		$loweredMessage = \strtolower($commitMessage);
+
+		if (\str_starts_with($loweredMessage, 'merge')) {
+			continue;
+		}
+
+		$isValid = preg_match($pattern, $commitMessage);
+
+		if ($isValid) {
+			echo "Conventional commits in $id: OK\n";
+		} else {
+			echo "Conventional commits in $id: ERR\n";
+
+			$invalidCommits[] = $id;
+		}
+	}
+} else {
 	exit(0);
 }
 
-// Ověření formátu conventional commits
-$pattern = '/^(feat|fix|docs|style|refactor|test|chore|perf|build|ops)(\([^)]+\))?!?: ((.|\n)+)/';
-$isValid = preg_match($pattern, $commitMessage);
-
-// Výsledek
-if ($isValid) {
-	echo "Conventional commits in last commit: OK";
-} else {
-	echo "Conventional commits in last commit: ERR";
-}
-
-exit($isValid ? 0 : 1);
+exit($invalidCommits ? 1 : 0);
